b3ccb78310344f0f4d548e56ecc9e432aebb31b9
DerekPeacock <derek.peacock@yahoo.com>
2020-08-13 19:23:31 +0100

    Added Unit Test project

---
+++ ConsoleHelpers.Test/DistanceConverterTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleHelpers.Test
+{
+    [TestClass]
+    public class DistanceConverterTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
